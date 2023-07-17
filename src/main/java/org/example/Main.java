559fbfb10d60a5d8a423b826b58fba9c8aa6f741
Queentart <alsdud9025@naver.com>
2023-07-18 08:51:59 +0900

    third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,10 +18,17 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         // 이미지 파일 경로
-        String imagePath = "C:\\JAVA_OCR\\images\\english_sentence4.png";
+        String imagePath = "C:\\JAVA_OCR\\images\\japanese_sentence2.png";
 
         // Tesseract OCR 인스턴스 생성
         ITesseract tesseract = new Tesseract();
+
+        // OCR 언어 설정
+        // 다수의 언어 설정 (예: 영어, 프랑스어, 독일어)
+        String[] languages = {"eng", "fra", "deu", "chi_sim", "jpn"};
+        for (String language : languages) {
+            tesseract.setLanguage(language);
+        }
 
         try {
             // 이미지 파일로부터 텍스트 추출
@@ -31,6 +38,7 @@ public class Main {
             // Google Translation API JSON 키 파일 경로
             // 업로드 시 빼고 업로드 요망 - String jsonKeyFilePath 변수명은 쓰되, "" 안에 있는 글자는 명시하지 말 것.
             // 이 주석 바로 밑에 작성(변수명: String jsonKeyFilePath)
+            String jsonKeyFilePath = "C:\\translation_api_key\\spherical-entry-391823-2f5e9256772c.json";
 
             // JSON 키 파일을 사용하여 인증 정보 로드
             GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(jsonKeyFilePath));
@@ -38,21 +46,28 @@ public class Main {
             // Translate 서비스 클라이언트 생성
             Translate translate = TranslateOptions.newBuilder().setCredentials(credentials).build().getService();
 
-            // 추출된 텍스트를 번역
-            String sourceLanguage = "en"; // 추출된 텍스트의 언어 (예: 영어)
-            String targetLanguage = "ko"; // 번역할 언어 (예: 한국어)
-            Translation translation = translate.translate(extractedText, Translate.TranslateOption.sourceLanguage(sourceLanguage), Translate.TranslateOption.targetLanguage(targetLanguage));
+            // 텍스트 언어 감지
+            String detectedLanguage = detectLanguage(extractedText, translate);
+
+            // 번역 서비스 호출하여 번역
+            Translation translation = translate.translate(extractedText, Translate.TranslateOption.targetLanguage("ko"));
 
             // 디코딩된 텍스트
             String decodedText = StringEscapeUtils.unescapeHtml4(translation.getTranslatedText());
 
             // 추출된 텍스트 출력
             System.out.println("Extracted Text: " + extractedText);
+            System.out.println("Detected Language: " + detectedLanguage);
 
             // 디코딩된 번역 결과 출력
             System.out.println("Translated Text: " + decodedText);
         } catch (TesseractException | IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String detectLanguage(String textToDetect, Translate translate) {
+        // 텍스트 언어 감지
+        return translate.detect(textToDetect).getLanguage();
     }
 }
